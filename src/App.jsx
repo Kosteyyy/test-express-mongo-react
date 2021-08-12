711d23f7b5ff8f8e8f9b2ded5e0f803cbc292a73
Kosteyyy <averkiev@yandex.ru>
2021-08-12 13:47:01 +0300

    Configured react app with server v.0

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./styles.css";
+
+const App = () => <h1>Hello World!!! This is new</h1>
+
+ReactDOM.render(<App />, document.getElementById('root'));
